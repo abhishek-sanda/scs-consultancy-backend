ef3b78c346d3b9268ea44b349b6415e5d30bb369
abhishek-sanda <abhisheksanda@gmail.com>
2025-12-25 11:05:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin:https://consultancy-project-frontend-eight.vercel.app
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
